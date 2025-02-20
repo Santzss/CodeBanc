761b8a8131b07168dad7b21a2b72f397c770a8b0
Rayan Santos <rayansantos0088@gmail.com>
2025-02-19 22:18:29 -0300

    codigo banco aprimorado

---
+++ src/Aula05/Aula05.java
@@ -0,0 +1,85 @@
+package Aula05;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula05 {
+    public static void main(String[] args) {
+
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        ContaBanco p1 = null;
+
+        while (true) {
+            System.out.println("===== SEJA BEM VINDO =====");
+            System.out.println("Digite o que deseja:  ");
+            System.out.println("1 - abrir conta ");
+            System.out.println("2 - fechar conta ");
+            System.out.println("3 - depositar ");
+            System.out.println("4 - sacar ");
+            System.out.println("5 - pagamento mensal");
+            System.out.println("6 - sair");
+            System.out.println("=================");
+            int opcao = sc.nextInt();
+
+
+            if (opcao == 1) {
+                sc.nextLine();
+                System.out.println("Digite seu nome: ");
+                String d = sc.nextLine();
+
+                System.out.println("Digite o numero da conta: ");
+                int c = sc.nextInt();
+
+                sc.nextLine();
+
+                System.out.println("Selecione o tipo da conta: 1 - CC(conta corrente) / 2 - CP(conta poupança)");
+                String tipoEscolhido = sc.nextLine();
+
+                String t = tipoEscolhido.equals("1") ? "CC" : "CP";
+
+                p1 = new ContaBanco(d, c, t);
+                p1.abrirConta(t);
+                p1.estadoAtual();
+
+            } else if (opcao == 2) {
+                if (p1 != null) {
+                    p1.fecharConta();
+                } else {
+                    System.out.println("Nenhuma conta encontrada no sistema!");
+                }
+            } else if (opcao== 3) {
+                if (p1 != null) {
+                    System.out.println("Digite o valor do deposito: ");
+                    float v = sc.nextFloat();
+                    p1.depositar(v);
+                    p1.estadoAtual();
+                } else {
+                    System.out.println("Nehuma conta encontrada no sistema!");
+                }
+            } else if (opcao == 4) {
+                if (p1 != null) {
+                    System.out.println("Digite o valor de saque: ");
+                    float v = sc.nextFloat();
+                    p1.sacar(v);
+                    p1.estadoAtual();
+                } else {
+                    System.out.println("Nenhuma conta encontrada no sistema! ");
+                }
+            } else if (opcao == 5) {
+                if (p1 != null){
+                    p1.pagarMensal();
+                    p1.estadoAtual();
+                } else  {
+                    System.out.println("Nenhuma conta encontrada no sistema!");
+                }
+            } else if (opcao == 6) {
+                System.out.println("Obrigado, volte sempre!");
+                break;
+            }
+
+
+        }
+    }
+}
